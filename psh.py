bb4d67690140c3946b12938ceb3e6d97281aa3e1
Bidisha Mukherjee <mukherjeebidisha94@gmail.com>
2013-07-19 09:21:31 +0530

    Adds the greet and stock commands

---
+++ psh.py
@@ -0,0 +1,32 @@
+from cmd2 import Cmd
+from getpass import getuser
+import requests
+import sys
+
+__version__ = '0.1'
+
+class Application(Cmd):
+    """
+    The main Application class
+    """
+
+    def __init__(self):
+    Cmd.__init__(self)
+
+    def do_hello(self, line):
+    print "Hello:", line
+
+    def do_sayit(self,line):
+    print "Python Rocks!"
+
+    def do_greet(self,line):
+    print "hi,%s" % (getuser())
+    
+    def do_stockgoog(self,line):
+    a = requests.get("http://download.finance.yahoo.com/d/quotes.csv?s=GOOG&f=l1"
+    value=link.text
+    print value
+
+if __name__ == '__main__':
+    app = Application()
+    app.cmdloop()
